33edc3b144932852d7218d80040327acd27e6c6d
Tushar Patel <tpatel@codal.com>
2021-10-28 14:43:39 +0530

    Added book

--- src/index.js
+++ src/index.js
@@ -2,12 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(<Greetings/>,
-    document.getElementById('root')
-);
 
-function Greetings() {
-    return <h2> Greeting From All of Us!!</h2>
+function BookList() {
+    return (
+        <div>
+            <BookName/>
+        </div>
+    )
+
 }
 
+const BookName = () => {
+    return <div>
+        <img src="https://m.media-amazon.com/images/I/41gr3r3FSWL._SY346_.jpg" alt="book"/>
+        <div>Book1</div>
+        <BookDescription/>
+    </div>
+}
+const BookDescription = () => {
+    return <div>
+        <p> Here is the Description</p>
+    </div>
+}
+ReactDOM.render(<BookList/>,
+    document.getElementById('root')
+);
 reportWebVitals(console.log);
