44084852a83c28500505bef96b856bdbd53dab22
Tushar Patel <tpatel@codal.com>
2021-10-29 14:27:43 +0530

    Experimenting with CSS

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
+import './index.css'
+
 
 
 function BookList() {
     return (
-        <div>
+        <div className="booklist">
+            <BookName/>
+            <BookName/>
+            <BookName/>
+            <BookName/>
+            <BookName/>
             <BookName/>
         </div>
     )
@@ -13,9 +20,9 @@ function BookList() {
 }
 
 const BookName = () => {
-    return <div>
+    return <div className='book'>
         <img src="https://m.media-amazon.com/images/I/41gr3r3FSWL._SY346_.jpg" alt="book"/>
-        <div>Book1</div>
+        <h1>Book1</h1>
         <BookDescription/>
     </div>
 }
